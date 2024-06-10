b815f88eb31b5be96cea510d9be9da3a9adc13a6
Tim Powell <timothy.powell@eviden.com>
2024-06-10 14:39:22 +0100

    Add privacy and accessibility links and customer service contact info to footer

---
+++ src/FrontendRegulatorAccountEnrollment.Web/Configs/FooterOptions.cs
@@ -0,0 +1,13 @@
+namespace FrontendRegulatorAccountEnrollment.Web.Configs
+{
+    public class FooterOptions
+    {
+        public const string ConfigSection = "FooterDetails";
+
+        public string PrivacyLink { get; set; }
+        public string CookiesLink { get; set; }
+        public string AccessibilityLink { get; set; }
+        public string CustomerServiceEmail { get; set; }
+        public string CustomerServicePhone { get; set; }
+    }
+}
